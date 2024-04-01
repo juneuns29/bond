b6434b96f5c94e5a49eb4b90c4661dff79d02cd6
juneuns29 <juneuns@gmail.com>
2024-04-01 12:46:26 +0900

    방명록 작성글 카운트 추가

--- bond/src/bond/sql/GboardSQL.java
+++ bond/src/bond/sql/GboardSQL.java
@@ -2,13 +2,14 @@ package bond.sql;
 
 public class GboardSQL {
 	public final int SEL_ALL_LIST 	= 1001;
+	public final int SEL_ID_CNT 	= 1002;
 	
 	public String getSQL(int code) {
 		StringBuffer buff = new StringBuffer();
 		switch(code) {
 		case SEL_ALL_LIST:
 			buff.append("SELECT ");
-			buff.append("	mno, id, filename sname, wdate, body ");
+			buff.append("	gno, mno, id, filename sname, wdate, body ");
 			buff.append("FROM ");
 			buff.append("	member, avatar, gboard ");
 			buff.append("WHERE ");
@@ -16,6 +17,15 @@ public class GboardSQL {
 			buff.append("	AND	avatar = ano ");
 			buff.append("ORDER BY ");
 			buff.append("	wdate DESC ");
+			break;
+		case SEL_ID_CNT:
+			buff.append("SELECT ");
+			buff.append("    COUNT(*) cnt ");
+			buff.append("FROM ");
+			buff.append("    member, gboard ");
+			buff.append("WHERE ");
+			buff.append("    mno = writer ");
+			buff.append("    AND id = ? ");
 			break;
 		}
 		
--- bond/src/bond/vo/BoardVO.java
+++ bond/src/bond/vo/BoardVO.java
@@ -1,5 +1,91 @@
 package bond.vo;
 
-public class BoardVO {
+import java.text.*;
+import java.sql.*;
+import java.util.Date;
 
+/**
+ * 이 클래스는 게시판 데이터 한개를 기억할 수 있는 클래스
+ * @author	전은석
+ * @since	2024.04.01
+ * @version	v.1.0
+ * 			2024.04.01	- 클래스 제작 [ 담당자: 전은석 ]
+ *
+ */
+public class BoardVO {
+	private int gno, mno, ano, cnt;
+	private String id, body, sdate, sname;
+	private Date wdate;
+	private Time wtime;
+	public int getGno() {
+		return gno;
+	}
+	public void setGno(int gno) {
+		this.gno = gno;
+	}
+	public int getMno() {
+		return mno;
+	}
+	public void setMno(int mno) {
+		this.mno = mno;
+	}
+	public int getAno() {
+		return ano;
+	}
+	public void setAno(int ano) {
+		this.ano = ano;
+	}
+	public int getCnt() {
+		return cnt;
+	}
+	public void setCnt(int cnt) {
+		this.cnt = cnt;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getBody() {
+		return body;
+	}
+	public void setBody(String body) {
+		this.body = body;
+	}
+	public String getSdate() {
+		return sdate;
+	}
+	public void setSdate() {
+		SimpleDateFormat form1 = new SimpleDateFormat("yyyy.MM.dd ");
+		SimpleDateFormat form2 = new SimpleDateFormat("HH:mm:ss");
+		sdate = form1.format(wdate) + form2.format(wtime);
+	}
+	public void setSdate(String sdate) {
+		this.sdate = sdate;
+	}
+	public String getSname() {
+		return sname;
+	}
+	public void setSname(String sname) {
+		this.sname = sname;
+	}
+	public Date getWdate() {
+		return wdate;
+	}
+	public void setWdate(Date wdate) {
+		this.wdate = wdate;
+	}
+	public Time getWtime() {
+		return wtime;
+	}
+	public void setWtime(Time wtime) {
+		this.wtime = wtime;
+		setSdate();
+	}
+	@Override
+	public String toString() {
+		return "BoardVO [gno=" + gno + ", mno=" + mno + ", ano=" + ano + ", cnt=" + cnt + ", id=" + id + ", body="
+				+ body + ", sdate=" + sdate + ", sname=" + sname + ", wdate=" + wdate + ", wtime=" + wtime + "]";
+	}
 }
